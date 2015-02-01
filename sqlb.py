7794c684bcbec852f0fe26ac9e7fc6f4495dd956
BRENT MACDONALD <abrentmac2@outlook.com>
2015-01-31 22:22:51 -0500

    learned executemany and read from csv

--- sqlb.py
+++ sqlb.py
@@ -1,20 +1,28 @@
 #INSERT command
 
-# impor the sqlite3 library
+#Note that this demonstrates two different ways to populate a table, the first does not use the 'with' keyword; the second does
+
+# import the sqlite3 library
 import sqlite3
 
-# create the connection object
-conn = sqlite3.connect("new.db")
+## create the connection object
+#conn = sqlite3.connect("new.db")
 
-# get a cursor object used to execute SQL commands
-cursor = conn.cursor()
+## get a cursor object used to execute SQL commands
+#cursor = conn.cursor()
 
-# insert data
-cursor.execute("INSERT INTO population VALUES('New York City', 'NY', 8200000)")
-cursor.execute("INSERT INTO population VALUES('San Francisco', 'CA', 800000)")
+## insert data
+#cursor.execute("INSERT INTO population VALUES('New York City', 'NY', 8200000)")
+#cursor.execute("INSERT INTO population VALUES('San Francisco', 'CA', 800000)")
 
-# commit the changes
-conn.commit()
+## commit the changes
+#conn.commit()
 
-# close the database connection 
-conn.close()
+## close the database connection 
+#conn.close()
+
+with sqlite3.connect("new.db") as connection:
+    c = connection.cursor()
+    c.execute("INSERT INTO population VALUES('Toronto', 'ON', 521232)")
+    c.execute("INSERT INTO population VALUES('Vancouver', 'BC', 2323423)")
+
